57b36929e5e10e61ec51158ea8db558c4c1a0b94
Girish Bhanarkar <girish@example.com>
2016-09-28 14:21:21 +0530

    pro change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = turned
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -88,5 +88,5 @@ Rails.application.configure do
   authentication:       'plain',
   enable_starttls_auto: true  }
 
-  config.action_mailer.default_url_options ={host: "https://pure-oasis-99757.herokuapp.com"}
+  config.action_mailer.default_url_options ={"https://pure-oasis-99757.herokuapp.com"}
 end
